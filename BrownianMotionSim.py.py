e870bd77448e19d8b9a830e1eabf3385d47bc2bf
Kish436 <kishanmodha@outlook.com>
2026-07-20 15:46:40 +0100

    Implement GBM and update plotting for stock prices
    Added Geometric Brownian Motion function and stock price plotting.

--- BrownianMotionSim.py.py
+++ BrownianMotionSim.py.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import yfinance as yf
 
 def BrownianMotion(t, nSteps, nPaths):
     dt=t/nSteps
@@ -9,11 +10,22 @@ def BrownianMotion(t, nSteps, nPaths):
     T=np.linspace(0,t,nSteps+1)
     return T,W
 
-t,W=BrownianMotion(1,1000,1)
-plt.plot(t,W.T)
+def GBM(t, nSteps, nPaths, S0, mu, sigma):
+
+    t,W=BrownianMotion(1,1000,1)
+    St = S0*np.exp((mu-0.5*sigma**2)*t+sigma*W)
+    return t, St
+
+T,S = GBM(t=1, nSteps=1000, nPaths=5, S0=100, mu=0.08, sigma=0.2)
+
+def StockPrices(ticker):
+
+
+plt.plot(T,S.T)
 plt.xlabel('Time')
-plt.ylabel('W Transposed')
+plt.ylabel('Price')
 plt.title('Brownian Motion')
 plt.show()
+
 
 
